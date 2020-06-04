d8e2df9a43ed53b9e073787dcdb6f027a9cde5ac
cezary-gawronski <idetepowiec@gmail.com>
2020-06-04 20:18:23 +0200

    finished first version my minigame

--- script.js
+++ script.js
@@ -5,20 +5,38 @@ let startButton = document.querySelector(".gameButton");
 
 let cards = document.querySelectorAll(".card");
 let randomCard = cards[Math.floor(Math.random() * cards.length)];
-console.log(randomCard);
+// console.log(randomCard);
+
+let positiveScore = document.querySelector(".face--positiveScore");
+console.log(positiveScore);
+let negativeScore = document.querySelector(".face--negativeScore");
+console.log(negativeScore);
+let successAnim = document.querySelector(".card");
+console.log(successAnim);
+
+
+let positiveScores = 0;
+let negativeScores = 0;
 
 for (let card of cards) {
     card.addEventListener("click", () => {
+        card.classList.remove("card--cardSuccess");
         if (startGame) {
             let randomCard = cards[Math.floor(Math.random() * cards.length)];
             console.log(randomCard);
             let drawnCard = randomCard.classList;
-            console.log(`Wylosowana karta to ${drawnCard}`);
-            console.log(`${card.classList}`);
+            // console.log(`Wylosowana karta to ${drawnCard}`);
+            // console.log(`${card.classList}`);
             if (drawnCard == card.classList) {
                 console.log("SUKCESS");
+                positiveScores += 1;
+                positiveScore.innerText = positiveScores;
+                randomCard.classList.add("card--cardSuccess");
             } else {
+                negativeScores += 1;
+                negativeScore.innerText = negativeScores;
                 console.log("PORAŻKA");
+                card.classList.remove("card--cardSuccess");
 
             }
         } else {
@@ -30,7 +48,7 @@ for (let card of cards) {
 
 startButton.addEventListener("click", () => {
     startGame = true;
-    document.querySelector(".gameBox__command").innerHTML = "GRAMY! Wybierz Kartę!";
+    document.querySelector(".gameBox__command").innerText = "GRAMY! Wybierz Kartę!";
     document.documentElement.style.setProperty('--bgcAllert', 'unset');
     document.documentElement.style.setProperty('--colorAllert', 'unset');
     console.log(startGame);
@@ -39,18 +57,5 @@ startButton.addEventListener("click", () => {
 
 
 
-console.log(startGame);
-
-
-
-
-    // let cardOneClick =  
-    // let cardTwoClick =  
-    // let cardThreeClick =  
-    // console.log(randomCard);
-    // Wybierz jeden z poniższych kartoników
-    // console.log(card);
-    // console.log(card[0], card[1], card[2]);
-
-
+// console.log(startGame);
 
