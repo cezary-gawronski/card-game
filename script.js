677b1827edc9c9a13f3c1c856e4e7cb0ea249916
cezary-gawronski <idetepowiec@gmail.com>
2020-06-04 14:05:30 +0200

    added first js script :)

---
+++ script.js
@@ -0,0 +1,25 @@
+let card = document.querySelectorAll(".card");
+let randomCard = card[Math.floor(Math.random() * card.length)];
+
+let oldCommunique = document.querySelector(".gameBox__command").innerText;
+let newCommunique = "Wybierz jeden z poniższych kartoników";
+// console.log(oldCommunique);
+// console.log(newCommunique);
+
+let startGame = false;
+let startButton = document.querySelector(".gameButton");
+// console.log(startButton);
+
+
+
+
+
+
+
+
+// console.log(randomCard);
+
+
+// Wybierz jeden z poniższych kartoników
+// console.log(card);
+// console.log(card[0], card[1], card[2]);
