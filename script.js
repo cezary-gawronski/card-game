f6aa37b1394f08b33ab0e888d96fb96f3ad3ee7d
cezary-gawronski <idetepowiec@gmail.com>
2020-06-04 18:06:46 +0200

    first part working code :)

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
-let card = document.querySelectorAll(".card");
-let randomCard = card[Math.floor(Math.random() * card.length)];
 
-let oldCommunique = document.querySelector(".gameBox__command").innerText;
-let newCommunique = "Wybierz jeden z poniższych kartoników";
-// console.log(oldCommunique);
-// console.log(newCommunique);
 
 let startGame = false;
 let startButton = document.querySelector(".gameButton");
-// console.log(startButton);
+
+let cards = document.querySelectorAll(".card");
+let randomCard = cards[Math.floor(Math.random() * cards.length)];
+console.log(randomCard);
+
+for (let card of cards) {
+    card.addEventListener("click", () => {
+        if (startGame) {
+            let randomCard = cards[Math.floor(Math.random() * cards.length)];
+            console.log(randomCard);
+            let drawnCard = randomCard.classList;
+            console.log(`Wylosowana karta to ${drawnCard}`);
+            console.log(`${card.classList}`);
+            if (drawnCard == card.classList) {
+                console.log("SUKCESS");
+            } else {
+                console.log("PORAŻKA");
+
+            }
+        } else {
+            document.documentElement.style.setProperty('--bgcAllert', 'red');
+            document.documentElement.style.setProperty('--colorAllert', 'yellow');
+        }
+    });
+}
+
+startButton.addEventListener("click", () => {
+    startGame = true;
+    document.querySelector(".gameBox__command").innerHTML = "GRAMY! Wybierz Kartę!";
+    document.documentElement.style.setProperty('--bgcAllert', 'unset');
+    document.documentElement.style.setProperty('--colorAllert', 'unset');
+    console.log(startGame);
+
+});
+
+
+
+console.log(startGame);
 
 
 
 
+    // let cardOneClick =  
+    // let cardTwoClick =  
+    // let cardThreeClick =  
+    // console.log(randomCard);
+    // Wybierz jeden z poniższych kartoników
+    // console.log(card);
+    // console.log(card[0], card[1], card[2]);
 
 
 
-
-// console.log(randomCard);
-
-
-// Wybierz jeden z poniższych kartoników
-// console.log(card);
-// console.log(card[0], card[1], card[2]);
